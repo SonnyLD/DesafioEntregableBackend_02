7cf10b85dcd82301a741d61346ac05fdaa898ff8
Sonny Del Valle <sonny@CTC06C12780LP.tim.com.pe>
2022-12-09 18:54:52 -0500

    Metodos agregados

--- desafioEntregable_01.js
+++ desafioEntregable_01.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
+const { title } = require('process');
 
 
 class ProductManager {
     constructor() {
         this.getProducts = [];
     }
-    async addPtoducts(title, description, price, thumbnail, stock = 25) {
-
+    async addProducts(title, description, price, thumbnail, stock) {
       try{
        const producto ={
         id: this.#getMaxId() + 1,
@@ -17,44 +17,71 @@ class ProductManager {
         code: [],
         stock,
     };
-    if(fs.existsSync('producto.json')){
-      const producto = JSON.parse(await fs.promises.readFile('Producto.json','utf8'));
-      this.getProducts = producto
+    this.getProducts.push(producto);
+    if(fs.existsSync('getProducts.json')){
+      const getProducts = JSON.parse(await fs.promises.readFile('getProducts.json','utf8'));
+      this.getProducts = getProducts;
       this.getProducts.push(producto);
     } else{
-      this.getProducts.push(producto); 
+      this.getProducts.push(producto);
     }
-      fs.promises.writeFile('Producto.json',JSON.stringify(this.getProducts))
+
+    fs.promises.writeFile('getProducts.json',JSON.stringify(this.getProducts))
     }catch (error) {
-      console.log(error)
-      throw new Error(error)
+      console.log(error);
+      throw new Error(error);
     }
 }
-    getProductById(idProducto, idNCode) {
-    const producto = this.#getProducts(idProducto);
-    if (producto) {
-      if (!producto.code.includes(idNCode)) producto.code.push(idNCode);
-      console.log(this.getProducts);
-    } else {
-      console.log("El producto no existe");
-    }
-    }
-  
+
+getProductById(idProducto) {
+  const producto = this.#getProducts(idProducto);
+  if (producto) {
+    if (!producto.includes(idProducto)) producto.push(idProducto);
+    console.log(this.getProducts);
+  } else {  
+     console.log("El producto existe");
+  }
+}
 #getMaxId() {
-    let maxId = 0;
-    this.getProducts.map((producto) => {
-      if (producto.id > maxId) maxId = producto.id;
-    });
-    return maxId;
+  let maxId = 0;
+  this.getProducts.map((producto) => {
+    if (producto.id > maxId) maxId = producto.id
+  });
+  return maxId;
+}
+#getProducts(idProducto) {
+  return this.getProducts.find((producto) => producto.id === idProducto);
+}
+
+updateProduct(idProducto) {
+  const producto = this.getProductById(this.producto);
+  if (producto) {
+    idProducto.title = this.title;
+    idProducto.description = this.description;
+  } else{
+    console.log("el producto no existe");
+  }
+ 
+}
+
+deleteProduct(idProducto){
+  const producto = this.getProductById(this.idProducto);
+  if (producto) {
+    idProducto.title = this.title;
+   
+  }else{
+   console.log(title);
   }
 
-  #getProducts(idProducto) {
-    return this.getProducts.find((producto) => producto.id === idProducto);
-  }  
-
 }
 
+}
 const productManager = new ProductManager();
 
-productManager.addPtoducts("Mesa de comedor", "mesa de madera", 1400, "Sin imagen", 25);
-productManager.addPtoducts("Escritorio", "Producto de caoba", 2000, "Sin imagen", 30);
+productManager.addProducts("Mesa de comedor", "mesa de madera", 1400, "Sin imagen", 60);
+productManager.addProducts("Sofa de sala", "Producto", 1000, "Sin imagen", 100);
+productManager.getProductById(4);
+productManager.updateProduct("");
+productManager.deleteProduct(1, title);
+
+
